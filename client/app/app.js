b94ebd4dffec7c7c01365355df8e791dfe5b74e5
D Shaffer <404shaff@gmail.com>
2016-06-04 18:01:04 -0400

    added bower components

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('1vSubShopApp', ['1vSubShopApp.auth', '1vSubShopApp.admin', '1vSubShopApp.constants',
-    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
-    'validation.match'
+angular.module('1vSubShopApp', [
+                '1vSubShopApp.auth',
+                '1vSubShopApp.admin',
+                '1vSubShopApp.constants',
+                'ngCookies',
+                'ngResource',
+                'ngSanitize',
+                'btford.socket-io',
+                'ui.router',
+                'ui.bootstrap',
+                'validation.match',
+                'ngAnimate'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
-
     $locationProvider.html5Mode(true);
   });
