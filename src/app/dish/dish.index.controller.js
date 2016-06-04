7cbf868a7c54c9d59174028d8060fa93f3d9febd
D Shaffer <404shaff@gmail.com>
2016-06-04 15:14:55 -0400

    added search items

--- src/app/dish/dish.index.controller.js
+++ src/app/dish/dish.index.controller.js
@@ -15,4 +15,25 @@
       $state.go( 'dishDetail', { dishId : dish.id } );
     };
   }
+  angular
+    .module('1vSubShop')
+    .filter('inventory', function() {
+
+    function isMatch(str, pattern) {
+      return str.toLowerCase().indexOf(pattern.toLowerCase()) !== -1;
+    }
+
+    return function(inventory, searchText) {
+      var dishes = {
+          searchText: searchText,
+          out: []
+      };
+      angular.forEach(inventory, function (dish) {
+        if (isMatch(dish.name, this.searchText) ) {
+          this.out.push(dish);
+        }
+      }, dishes);
+      return dishes.out;
+    };
+  });
 })();
