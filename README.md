eb5c08eadf1ffb1ca98a7a00300d2f19596c7b16
D Shaffer <404shaff@gmail.com>
2016-06-04 17:38:46 -0400

    reset

--- README.md
+++
@@ -1,1 +0,0 @@
-#Sub Shop Supervisor V1
--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('1vSubShop', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
