2069e4aecaa53f36822744255aa4613e18fc2082
D Shaffer <404shaff@gmail.com>
2016-06-05 13:19:36 -0400

    add bower components

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("1vSubShopApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,16 @@
 'use strict';
 
-angular.module('1vSubShopApp', ['1vSubShopApp.constants', 'ngCookies', 'ngResource', 'ngSanitize',
-    'btford.socket-io', 'ui.router', 'ui.bootstrap'
+angular.module('1vSubShopApp', [
+                '1vSubShopApp.constants',
+                'ngCookies',
+                'ngResource',
+                'ngSanitize',
+                'btford.socket-io',
+                'ui.router',
+                'ui.bootstrap',
+                'ngAnimate'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
-
     $locationProvider.html5Mode(true);
   });
